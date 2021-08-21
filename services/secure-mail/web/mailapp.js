30b76d574dea0e4c40ae434c08aacd152d5db0ad
art <zinenkoartem@gmail.com>
2021-08-21 22:26:59 +0500

    Reworked web

---
+++ services/secure-mail/web/mailapp.js
@@ -0,0 +1,46 @@
+const term = new Terminal();
+term.setOption('cursorBlink', true);
+term.open(document.getElementById('terminal'));
+term.prompt = () => {
+    term.write('\r\n$ ');
+};
+
+const ws = new WebSocket('ws://localhost:8080/');
+
+ws.onopen = () => {
+    term.writeln('Welcome to xterm.js');
+    term.prompt();
+};
+
+var cmd = '';
+
+ws.onmessage = message => {
+  term.write(`\r\n${message.data}`);
+  term.prompt();
+  cmd = '';
+};
+
+term.onData(e => {
+    switch (e) {
+        case '\r':
+            ws.send(cmd);
+            break;
+        case '\u0003':
+            term.prompt();
+            cmd = '';
+            break;
+        case '\u007F':
+            if (term._core.buffer.x > 2) {
+                cmd = cmd.slice(0, -1);
+                term.write('\b \b');
+            }
+            break;
+        case '\f':
+            term.clear();
+            cmd = '';
+            break;
+        default:
+            cmd += e;
+            term.write(e);
+    }
+});
--- services/secure-mail/web/server.js
+++ services/secure-mail/web/server.js
@@ -1,41 +1,46 @@
 const http = require('http');
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
+const WebSocket = require('ws');
 
 const requestListener = function (request, response) {
-    var filePath = '';
-    var contentType = '';
+    const routes = {
+        '/' : {filePath : 'index.html', contentType : 'text/html'},
+        '/xterm.css' : {filePath : 'node_modules/xterm/css/xterm.css', contentType : 'text/css'},
+        '/xterm.js' : {filePath : 'node_modules/xterm/lib/xterm.js', contentType : 'text/javascript'},
+        '/xterm.js.map' : {filePath : 'node_modules/xterm/lib/xterm.js.map', contentType : 'application/json'},
+        '/mailapp.js' : {filePath : 'mailapp.js', contentType : 'text/javascript'}
+    };
 
-    switch (request.url) {
-        case '/':
-            filePath = 'index.html';
-            contentType = 'text/html';
-            break;
-        case '/xterm.css':
-            filePath = 'node_modules/xterm/css/xterm.css';
-            contentType = 'text/css';
-            break;
-        case '/xterm.js':
-            filePath = 'node_modules/xterm/lib/xterm.js';
-            contentType = 'text/javascript';
-            break;
-        default:
-            response.writeHead(404);
-            response.end();
-            return;
+    if (!(request.url in routes)) {
+        response.writeHead(404);
+        response.end();
+        return;
     }
 
-    fs.readFile(filePath, function (error, content) {
+    const route = routes[request.url];
+ 
+    fs.readFile(route.filePath, function (error, content) {
         if (error) {
             response.writeHead(500);
             response.end();
         }
         else {
-            response.writeHead(200, { 'Content-Type': contentType });
+            response.writeHead(200, { 'Content-Type': route.contentType });
             response.end(content, 'utf-8');
         }
     });
 }
 
 const server = http.createServer(requestListener);
+const wsServer = new WebSocket.Server({server: server});
+wsServer.on('connection', function connection(ws) {
+    console.log(`Connection`);
+
+    ws.on('message', function incoming(message) {
+        console.log(`received: ${message}`);
+        ws.send("OUTPUT");
+    });
+});
+
 server.listen(8080);
