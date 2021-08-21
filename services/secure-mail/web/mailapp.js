60d1b7659ce5e4d5cd7130b63d02faee02d80958
art <zinenkoartem@gmail.com>
2021-08-22 01:11:50 +0500

    Added templates for login and adduser

--- services/secure-mail/web/mailapp.js
+++ services/secure-mail/web/mailapp.js
@@ -13,6 +13,12 @@ ws.onopen = () => {
 };
 
 var cmd = '';
+var username = '';
+var password = '';
+var needUsername = false;
+var needPassword = false;
+
+// TODO: message: {exit_code: ..., output: ...}
 
 ws.onmessage = message => {
   term.write(`\r\n${message.data}`);
@@ -20,27 +26,83 @@ ws.onmessage = message => {
   cmd = '';
 };
 
+const startEnterUsername = () => {
+    needUsername = true;
+    term.write(`\r\nUser: `);
+};
+const cleanupUsername = () => {
+    needUsername = false;
+    username = '';
+};
+
+const startEnterPassword = () => {
+    needPassword = true;
+    term.write(`\r\nPassword: `);
+};
+const cleanupPassword = () => {
+    needPassword = false;
+    password = '';
+};
+
 term.onData(e => {
     switch (e) {
         case '\r':
-            ws.send(cmd);
+            if (needUsername) {
+                cmd += ' ' + username;
+                cleanupUsername();
+                startEnterPassword();
+            } else if (needPassword) {
+                cmd += ' ' + password;
+                cleanupPassword();
+                ws.send(cmd);
+            } else {
+                if ((cmd.startsWith('login') || cmd.startsWith('adduser')) && cmd.split(' ').length != 3) {
+                    if (cmd.split(' ').length == 1) {
+                        startEnterUsername();
+                    } else {
+                        startEnterPassword();
+                    }
+                } else {
+                    ws.send(cmd);
+                }
+            }
             break;
         case '\u0003':
-            term.prompt();
             cmd = '';
+            cleanupUsername();
+            cleanupPassword();
+            term.prompt();
             break;
         case '\u007F':
             if (term._core.buffer.x > 2) {
-                cmd = cmd.slice(0, -1);
+                if (needUsername) {
+                    username = username.slice(0, -1);
+                } else if (needPassword) {
+                    password = password.slice(0, -1);
+                } else {
+                    cmd = cmd.slice(0, -1);
+                }
                 term.write('\b \b');
             }
             break;
         case '\f':
-            term.clear();
-            cmd = '';
+            if (!needUsername && !needPassword) {
+                term.clear();
+                cmd = '';
+            }
             break;
         default:
-            cmd += e;
-            term.write(e);
+            if (needUsername) {
+                username += e;
+            } else if (needPassword) {
+                password += e;
+            } else {
+                cmd += e;
+            }
+            if (!needPassword) {
+                term.write(e);
+            } else {
+                term.write('*');
+            }
     }
 });
