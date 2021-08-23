7d17f0fecdc276013cefa8ae439426a2338fed69
art <zinenkoartem@gmail.com>
2021-08-24 01:27:40 +0500

    Rewrote checker.check()

--- checkers/secure-mail/smtp_client.py
+++ checkers/secure-mail/smtp_client.py
@@ -20,25 +20,25 @@ class SmtpClient:
     def send_secret_message(self, to, secret):
         msg = EmailMessage()
         msg["Subject"] = "Secret message"
-        msg["From"] = Address(username=str(uuid.uuid1()), domain=DOMAIN)
+        msg["From"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
 
         rnd = randint(0, 2)
         if rnd == 0:
             msg["To"] = Address(username=to, domain=DOMAIN)
             trace("'To' header will contain target username")
         else:
-            msg["To"] = Address(username=str(uuid.uuid1()), domain=DOMAIN)
+            msg["To"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
             if rnd == 1:
                 msg["Cc"] = Address(username=to, domain=DOMAIN)
                 trace("'Cc' header will contain target username")
 
                 if randint(0, 1) == 0:
-                    msg["Bcc"] = Address(username=str(uuid.uuid1()), domain=DOMAIN)
+                    msg["Bcc"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
             else:
                 msg["Bcc"] = Address(username=to, domain=DOMAIN)
                 trace("'Bcc' header will contain target username")
                 if randint(0, 1) == 0:
-                    msg["Cc"] = Address(username=str(uuid.uuid1()), domain=DOMAIN)
+                    msg["Cc"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
 
         text = ''
         html = ''
@@ -76,6 +76,61 @@ class SmtpClient:
 
         if attachment_data:
             msg.add_attachment(attachment_data.encode('utf-8'), maintype='application', subtype='octet-stream', filename='secret')
+
+        try:
+            trace("Connecting to %s:%i" % (self._host, PORT))
+            with smtplib.SMTP(self._host, PORT, timeout=TIMEOUT) as s:
+                trace("Sending email to %s@%s" % (to, DOMAIN))
+                s.send_message(msg)
+        except (socket.timeout, ConnectionRefusedError) as e:
+            verdict(DOWN, "SMTP connection error", "SMTP connection error: %s" % str(e))
+        except smtplib.SMTPException as e:
+            verdict(MUMBLE, "SMTP error", "SMTP error: %s" % str(e))
+
+    def send_phishing_message(self, to, link):
+        msg = EmailMessage()
+        msg["Subject"] = "Interesting business proposal"
+        msg["From"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
+
+        rnd = randint(0, 2)
+        if rnd == 0:
+            msg["To"] = Address(username=to, domain=DOMAIN)
+            trace("'To' header will contain target username")
+        else:
+            msg["To"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
+            if rnd == 1:
+                msg["Cc"] = Address(username=to, domain=DOMAIN)
+                trace("'Cc' header will contain target username")
+
+                if randint(0, 1) == 0:
+                    msg["Bcc"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
+            else:
+                msg["Bcc"] = Address(username=to, domain=DOMAIN)
+                trace("'Bcc' header will contain target username")
+                if randint(0, 1) == 0:
+                    msg["Cc"] = Address(username=str(uuid.uuid4()), domain=DOMAIN)
+
+        text = """\
+Dear Sir/Madam,
+I have an interesting business proposal I want to share with you.
+
+---
+Elon Musk"""
+
+        html = """\
+<html>
+  <body>
+    <p>Dear Sir/Madam,<br>
+       I have an <a href="%s">interesting business proposal</a> I want to share with you.<br><br>
+       ---<br>
+       Elon Musk
+    </p>
+  </body>
+</html>
+"""
+
+        msg.set_content(text)
+        msg.add_alternative(html % link, subtype='html')
 
         try:
             trace("Connecting to %s:%i" % (self._host, PORT))
