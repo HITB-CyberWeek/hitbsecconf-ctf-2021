88acde11c2d626bf40c5618d19a49c10e375178e
art <zinenkoartem@gmail.com>
2021-08-24 01:27:40 +0500

    Rewrote checker.check()

--- checkers/secure-mail/secure-mail.checker.py
+++ checkers/secure-mail/secure-mail.checker.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
   
 import sys
+import uuid
 import traceback
-import smtplib
-from email.message import EmailMessage
-from email.headerregistry import Address
-from email.utils import make_msgid
 import asyncio
-import websockets
 from smtp_client import SmtpClient
 from web_client import WebClient
 from user_db import UserDb
@@ -18,48 +14,22 @@ SMTP_PORT = 2525
 def info():
     verdict(OK, "vulns: 1")
 
-def check(args):
+async def check(args):
     if len(args) != 1:
         verdict(CHECKER_ERROR, "Wrong args count", "Wrong args count for check()")
     host = args[0]
     trace("check(%s)" % host)
 
-    msg = EmailMessage()
-    msg["Subject"] = "Interesting business proposal"
-    msg["From"] = Address("Elon Musk", "Elon.Musk", "tesla.com")
-    msg["To"] = Address("Receiver", "receiver", "hackerdom.com")
-    msg["Cc"] = Address("Receiver", "receiver1", "hackerdom.com")
-    msg["Bcc"] = Address("Receiver", "user", "hackerdom.com")
+    smtp_client = SmtpClient(host)
+    user_id = str(uuid.uuid4())
 
-    text = """\
-Dear Sir/Madam,
-I have an interesting business proposal I want to share with you.
-
----
-Elon Musk"""
-
-    html = """\
-<html>
-  <body>
-    <p>Dear Sir/Madam,<br>
-       I have an <a href="http://www.realpython.com">interesting business proposal</a> I want to share with you.<br><br>
-       ---<br>
-       Elon Musk
-    </p>
-  </body>
-</html>
-"""
-
-    msg.set_content(text)
-    msg.add_alternative(html, subtype='html')
-
-    filename = 'qqq'
-
-    with open('/etc/passwd', 'rb') as fp:
-        msg.add_attachment(fp.read(), maintype='application', subtype='octet-stream', filename=filename)
-
-    with smtplib.SMTP(host, SMTP_PORT) as s:
-        s.send_message(msg)
+    with UserDb() as db:
+        user = db.create_user(host, user_id)
+        async with WebClient(host) as wc:
+            await wc.create_user(user.name, user.password)
+        # TODO send random links
+        smtp_client.send_phishing_message(to=user.name, link='http://example.com')
+        # TODO check result
 
     sys.exit(OK)
 
@@ -94,7 +64,7 @@ def main(args):
         if args[0] == "info":
             info()
         elif args[0] == "check":
-            check(args[1:])
+            asyncio.get_event_loop().run_until_complete(check(args[1:]))
         elif args[0] == "put":
             asyncio.get_event_loop().run_until_complete(put(args[1:]))
         elif args[0] == "get":
