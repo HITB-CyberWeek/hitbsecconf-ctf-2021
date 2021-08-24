dae1d361c9df99791cd751c5deb3bf93a007f149
dimmo <dimmoborgir@gmail.com>
2021-08-25 02:00:34 +0500

    [fw] temporarily pass all traffic in xdp

--- services/fw/xdp_filter.c
+++ services/fw/xdp_filter.c
@@ -1,5 +1,6 @@
 #include <stddef.h>
 #include <string.h>
+#include <stdbool.h>
 #include <linux/bpf.h>
 #include <linux/in.h>
 #include <linux/if_ether.h>
@@ -9,40 +10,51 @@
 #include "bpf_helpers.h"
 #include "bpf_endian.h"
 
+//bool contains_id(void *ip, char *id) {
+//    return true;
+//}
+//
+//bool contains_pass(void *ip, unsigned int pass) {
+//    return *(int *)((char *)ip + 0x16) == pass;
+//}
+
 SEC("xdp_main")
 int  xdp_filter(struct xdp_md *ctx)
 {
-    void *data_end = (void *)(long)ctx->data_end;
-    void *data = (void *)(long)ctx->data;
+    char *data_end = (char *)(long)ctx->data_end;
+    char *eth = (char *)(long)ctx->data;
 //    void *udpdata;
-//    char *get = "GET cf5j-dxa9-6gpx";
+//    void *ip;
 
-    struct ethhdr *eth;
-    struct iphdr *ip;
-//    struct udphdr *udp;
-//    int i;
 
-    if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct udphdr) + 18 > data_end) {
-        return XDP_PASS; // Too short packet.
+    struct iphdr *ip_hdr;
+//    struct udphdr *udp_hdr;
+
+    char *ip = eth + sizeof(struct ethhdr);
+    if (ip > data_end) {
+        return XDP_PASS; // Not ethernet packet.
+    }
+    struct ethhdr *eth_hdr = (struct ethhdr *) eth;
+
+
+    if (eth_hdr->h_proto != bpf_htons(ETH_P_IP)) {
+        return XDP_PASS; // Unknown protocol.
     }
 
-    bpf_printk("Got packet %d (0x%x) bytes\n", data_end - data, data_end - data);
-
-    eth = data;
-    if (eth->h_proto != bpf_htons(ETH_P_IP)) {
+    ip_hdr = (void *)eth_hdr + sizeof(struct ethhdr);
+//    ip = (void *)ip_hdr;
+    if (ip_hdr->protocol != IPPROTO_UDP) {
         return XDP_PASS; // Invalid protocol.
     }
-    ip = (void *)eth + sizeof(struct ethhdr);
-    if (ip->protocol != IPPROTO_UDP) {
-        return XDP_PASS; // Invalid protocol.
-    }
+
+//    if (contains_id((void *)ip_hdr, "cf5j-dxa9-6gpx") && !contains_pass((void *)ip_hdr, 0x11223344)) {
+//        return XDP_DROP;
+//    }
 //    udp = (void *)ip + sizeof(struct iphdr); // FIXME: check ihl
 //    if (udp->dest != bpf_htons(17777)) {
 //        return XDP_PASS; // Invalid port.
 //    }
-    if (*(int *)((char *)ip + 0x16) != 0x44332211) {
-        return XDP_DROP;
-    }
+
 
 //    udpdata = (void *)udp + sizeof(struct udphdr);
 //    for (i=0; i < 18; i++) {
