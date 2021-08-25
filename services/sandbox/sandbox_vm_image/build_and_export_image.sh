e9dc818912f9bb9f0fc99820833813f70dd2d27f
Andrew Gein <andgein@yandex.ru>
2021-08-25 16:06:46 +0300

    [sandbox] Remove mf file due to the VirtualBox bug

--- services/sandbox/sandbox_vm_image/build_and_export_image.sh
+++ services/sandbox/sandbox_vm_image/build_and_export_image.sh
@@ -5,3 +5,6 @@ set -ex
 wget -N https://cdimage.debian.org/cdimage/archive/10.10.0/amd64/iso-cd/debian-10.10.0-amd64-netinst.iso
 
 packer build --force build.pkr.hcl
+
+# Due to https://www.virtualbox.org/ticket/19440
+rm output-sandbox/*.mf
