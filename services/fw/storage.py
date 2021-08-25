6d5ebfebcbbeaf5cf8ab2e3cc30c56b80f03111c
dimmo <dimmoborgir@gmail.com>
2021-08-26 02:37:39 +0500

    [fw] storage persists flags to disk

--- services/fw/storage.py
+++ services/fw/storage.py
@@ -1,32 +1,75 @@
 #!/usr/bin/env python3
-# Simple service stub to test checker code.
+# Legacy storage service.
+# Feel free to optimize :)
 
+import logging
+import os
 import socket
-port = 17777
 
-flags = dict()
+DATA_DIR = "/data/"
+PORT = 17777
+MAX_SIZE = 64
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind(("", port))
-print("Listening port {} ...".format(port))
-while True:
-    message, address = sock.recvfrom(1024)
-    message = message.decode("utf-8")
-    print("Received: {!r} from {!r}".format(message, address))
+
+def check_signature(a):
+    return True  # FIXME
+
+
+def load_flags():
+    flags = dict()
     try:
-        a = message.split(" ")
-        if a[0] == "PIN":
-            response = "PON"
-        elif a[0] == "PUT":
-            flags[a[1]] = a[2]
-            response = "OK"
-        elif a[0] == "GET":
-            response = flags.get(a[1], "NO")
-        elif a[0] == "DIR":
-            response = ",".join(sorted(flags.keys()))
-        else:
-            response = "UNK"
-    except Exception as e:
-        response = "ERR"
+        for fname in os.listdir(DATA_DIR):
+            full_name = os.path.join(DATA_DIR, fname)
+            if os.path.isfile(full_name) and os.path.getsize(full_name) <= MAX_SIZE:
+                with open(full_name) as f:
+                    data = f.read()
+                flags[fname] = data
+        logging.info("Loaded %d flags from disk", len(flags))
+    except Exception:
+        logging.exception("Loading flags from disk has failed")
+    return flags
 
-    sock.sendto(bytes(response, "utf-8"), address)
+
+def main():
+    logging.basicConfig(level=logging.INFO, format="[storage] %(asctime)s %(message)s")
+
+    flags = load_flags()
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(("", PORT))
+    logging.info("Listening UDP port {} ...".format(PORT))
+    while True:
+        message, address = sock.recvfrom(1024)
+        message = message.decode("utf-8")
+        logging.info("Received: {!r} from {!r}".format(message, address))
+        try:
+            a = message.split(" ")
+            if a[0] == "PIN":
+                response = "PON"
+            elif a[0] == "PUT":
+                if check_signature(a):
+                    flags[a[1]] = a[2]
+                    with open(os.path.join(DATA_DIR, a[1]), "w") as f:
+                        f.write(a[2])
+                    response = "OK"
+                else:
+                    response = "ERRSIG"
+            elif a[0] == "GET":
+                response = flags.get(a[1], "NO")
+            elif a[0] == "DIR":
+                if flags:
+                    response = ",".join(sorted(flags.keys()))
+                else:
+                    response = "EMPTY"
+            else:
+                response = "ERRCMD"
+        except Exception:
+            logging.exception("Exception")
+            response = "ERR"
+
+        logging.info("Sending response: %r", response)
+        sock.sendto(bytes(response, "utf-8"), address)
+
+
+if __name__ == "__main__":
+    main()
