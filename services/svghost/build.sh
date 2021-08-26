b9047ca6ef833f39492d644678a27948ba23cfe8
Dmitriy Titarenko <mail.dscheg@gmail.com>
2021-08-26 11:52:50 +0500

    [svghost] add sources

--- services/svghost/build.sh
+++ services/svghost/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker run --rm -v "$(pwd):/app" -w /app mcr.microsoft.com/dotnet/sdk:5.0 dotnet publish src/ -c release -o ./out
-rm -rf src/obj src/bin
+tar -zcf svghost.tar.gz --exclude="./src/bin" --exclude="./src/obj" --exclude="./src/data" --exclude="./src/settings" --exclude="./src/Properties" --exclude="./src/vs" --exclude="./src/idea" --exclude="*.user" --exclude="*.suo" ./src/
